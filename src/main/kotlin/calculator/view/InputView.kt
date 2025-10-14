8b2f4aad5dc2868f35a724df041d1e740d44a857
joon0447 <joon0447@naver.com>
2025-10-14 19:10:50 +0900

    feat : 문자열 입력 메시지 추가

--- src/main/kotlin/calculator/view/InputView.kt
+++ src/main/kotlin/calculator/view/InputView.kt
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Console
 class InputView {
 
     fun readInput(): String{
+        println("덧셈할 문자열을 입력해 주세요.")
         val input = Console.readLine()
         val delimiter = Delimiter()
         delimiter.parseCustomDelimiter(input)
