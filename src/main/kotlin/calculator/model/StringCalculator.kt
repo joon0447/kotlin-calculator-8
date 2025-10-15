7e5ef44e0f25e36aaa67f857afd0aa4aee185616
joon0447 <joon0447@naver.com>
2025-10-16 00:39:12 +0900

    feat(calculator) : 기존 문자열 순회 계산 방식에서 리스트 순회 계산 방식으로 개선

--- src/main/kotlin/calculator/model/StringCalculator.kt
+++ src/main/kotlin/calculator/model/StringCalculator.kt
@@ -2,10 +2,10 @@ package calculator.model
 
 class StringCalculator {
 
-    fun addNumbers(input : String) : Int {
+    fun addNumbers(input : List<String>) : Int {
         var total = 0
-        for(ch in input) {
-            total += ch.toString().toInt()
+        for(num in input) {
+            total += num.toInt()
         }
         return total
     }
