46b9fc3587082ada3c2a681c7e2fe7af1564fc32
joon0447 <joon0447@naver.com>
2025-10-15 23:06:59 +0900

    feat (delimiter) : 커스텀 구분자 정상 입력 확인 추가

--- src/main/kotlin/calculator/model/Delimiter.kt
+++ src/main/kotlin/calculator/model/Delimiter.kt
@@ -18,6 +18,16 @@ class Delimiter {
         delimiter += input
     }
 
+    fun isValidCustomDelimiterFormat(input: String): Boolean {
+        if(input.startsWith("//")){
+            val endIndex = input.indexOf("\n")
+            if(endIndex == -1 || endIndex <= 2) return false
+        }else{
+            if(input.contains("\n")) return false
+        }
+        return true
+    }
+
     fun getDelimiter() : String {
         return delimiter
     }
