133e71655fba891ec0846e56315279a78c9f51b1
joon0447 <joon0447@naver.com>
2025-10-17 00:52:43 +0900

    test(stringcalculator) : int -> BigInteger 변경에 따른 테스트 케이스 수정

--- src/test/kotlin/calculator/model/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/model/StringCalculatorTest.kt
@@ -9,8 +9,6 @@ class StringCalculatorTest {
     fun addNumbersTest() {
         val calculator = StringCalculator()
         val num = calculator.addNumbers(listOf("1", "105"))
-        assertEquals(106, num)
+        assertEquals(106.toBigInteger(), num)
     }
-
-
 }
