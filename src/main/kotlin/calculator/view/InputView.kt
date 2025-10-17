06a5f4343db1c29f733aaea61a58b840a5e1a2d3
joon0447 <joon0447@naver.com>
2025-10-18 00:13:34 +0900

    refactor(inputview) : 코딩 컨벤션에 따른 수정

--- src/main/kotlin/calculator/view/InputView.kt
+++ src/main/kotlin/calculator/view/InputView.kt
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
 
-    fun readInput(): String{
+    fun readInput(): String {
         println("덧셈할 문자열을 입력해 주세요.")
         val input = Console.readLine()
         return input
