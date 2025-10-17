5a4f6fa9f6535b420ee3a30a48ad0b8f015e249a
joon0447 <joon0447@naver.com>
2025-10-17 23:29:30 +0900

    test(delimiter) : 테스트 케이스 오타 수정

--- src/test/kotlin/calculator/model/DelimiterTest.kt
+++ src/test/kotlin/calculator/model/DelimiterTest.kt
@@ -41,8 +41,8 @@ class DelimiterTest {
     fun `커스텀 구분자 형식 확인` () {
         val delimiter = Delimiter()
         assertEquals(false, delimiter.isValidCustomDelimiterFormat("//;12312312"))
-        assertEquals(false, delimiter.isValidCustomDelimiterFormat(";\n12312312"))
-        assertEquals(true, delimiter.isValidCustomDelimiterFormat("//;\n1;2;3"))
+        assertEquals(false, delimiter.isValidCustomDelimiterFormat(";\\n12312312"))
+        assertEquals(true, delimiter.isValidCustomDelimiterFormat("//;\\n1;2;3"))
     }
 
 }
